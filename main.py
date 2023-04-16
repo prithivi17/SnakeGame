8d2afa2a9c8cf1b192359e481323409d779d59d6
Prithivi <prithivi.s101@gmail.com>
2023-04-16 16:30:35 +0530

    Changes added.

--- main.py
+++ main.py
@@ -39,14 +39,15 @@ while game_on:
 
     #detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        score.game_over()
-        game_on = False
+        score.reset_game()
+        snake.reset_snake()
+
 
     #detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            score.game_over()
+            score.reset_game()
+            snake.reset_snake()
 
 
 
--- snake.py
+++ snake.py
@@ -26,6 +26,13 @@ class Snake(Turtle):
         new_segment.goto(pos)
         self.segments.append(new_segment)
 
+    def reset_snake(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].pos())
 
